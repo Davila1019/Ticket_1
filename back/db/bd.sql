7e9d6d19d08dc313e5124c85877660f5817bbe28
Davila1019 <lalodavila41@gmail.com>
2021-09-29 19:51:41 -0500

    Querys

--- back/db/bd.sql
+++ back/db/bd.sql
@@ -1,5 +1,16 @@
-CREATE DATABASE ticket
+CREATE DATABASE ticket 
 GO
 
 USE ticket
-Hola mindo
+GO 
+
+CREATE TABLE users (
+	[name] VARCHAR(50) NOT NULL,
+	username VARCHAR(10) NOT NULL,
+	[password] VARCHAR(20) NOT NULL,
+	PRIMARY KEY(username)
+
+)
+INSERT INTO users ([name], username, [password]) VALUES ('Jesus Davila','lalodav19','octubre19')
+
+SELECT *  FROM users
