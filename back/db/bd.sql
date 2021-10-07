c1725f351d8c3b67ab402b8dc4e42d512bb9ee98
AleAlvanez <42542259+AleAlvanez@users.noreply.github.com>
2021-10-06 19:48:51 -0500

    Actualizacion de la base de datos

--- back/db/bd.sql
+++ back/db/bd.sql
@@ -12,5 +12,92 @@ CREATE TABLE users (
 
 )
 INSERT INTO users ([name], username, [password]) VALUES ('Jesus Davila','lalodav19','octubre19')
-
+GO
 SELECT *  FROM users
+GO
+CREATE TABLE presupuestos
+(
+  idPresupuesto INT NOT NULL IDENTITY (1,1),
+  creacion DATE NOT NULL,
+  proyecto VARCHAR (50) NOT NULL,
+  vers INT NOT NULL,
+  PRIMARY KEY (idPresupuesto)
+)
+CREATE TABLE mes
+(
+  idMes INT NOT NULL IDENTITY (1,1),
+  nombre_mes VARCHAR (20) NOT NULL,
+  PRIMARY KEY (idMes)
+)
+
+CREATE TABLE flujoEfec
+(
+  idFlujoE INT NOT NULL IDENTITY (1,1),
+  id_Presupuesto INT NOT NULL,
+  id_Mes INT NOT NULL,
+  ingreso FLOAT NOT NULL,
+  PRIMARY KEY (idFlujoE),
+    FOREIGN KEY(id_Presupuesto) REFERENCES presupuestos(idPresupuesto),
+	FOREIGN KEY(id_Mes) REFERENCES mes (idMes)
+)
+
+INSERT INTO presupuestos (creacion,proyecto,vers) VALUES ('05/10/21','BioProyect1',1);
+GO
+INSERT INTO mes(nombre_mes) VALUES ('Enero');
+INSERT INTO mes(nombre_mes) VALUES ('Febrero');
+INSERT INTO mes(nombre_mes) VALUES ('Marzo');
+INSERT INTO mes(nombre_mes) VALUES ('Abril');
+INSERT INTO mes(nombre_mes) VALUES ('Mayo');
+INSERT INTO mes(nombre_mes) VALUES ('Junio');
+INSERT INTO mes(nombre_mes) VALUES ('Julio');
+INSERT INTO mes(nombre_mes) VALUES ('Agosto');
+INSERT INTO mes(nombre_mes) VALUES ('Septiembre');
+INSERT INTO mes(nombre_mes) VALUES ('Octubre');
+INSERT INTO mes(nombre_mes) VALUES ('Noviembre');
+INSERT INTO mes(nombre_mes) VALUES ('Diciembre');
+
+GO
+CREATE TABLE recursos
+(
+  idRecurso INT NOT NULL IDENTITY (1,1),
+  id_Presupuesto INT NOT NULL,
+  id_Mes INT NOT NULL,
+  rol VARCHAR (30) NOT NULL,
+  recurso FLOAT NOT NULL,
+  PRIMARY KEY (idRecurso),
+    FOREIGN KEY(id_Presupuesto) REFERENCES presupuestos(idPresupuesto),
+	FOREIGN KEY(id_Mes) REFERENCES mes (idMes)
+)
+GO
+CREATE TABLE ingresos
+(
+  idIngreso INT NOT NULL IDENTITY (1,1),
+  id_Presupuesto INT NOT NULL,
+  id_Mes INT NOT NULL,
+  concepto FLOAT NOT NULL,
+  PRIMARY KEY (idIngreso),
+    FOREIGN KEY(id_Presupuesto) REFERENCES presupuestos(idPresupuesto),
+	FOREIGN KEY(id_Mes) REFERENCES mes (idMes)
+)
+
+CREATE TABLE costosDirectos
+(
+  idCostosD INT NOT NULL IDENTITY (1,1),
+  id_Presupuesto INT NOT NULL,
+  id_Mes INT NOT NULL,
+  concepto FLOAT NOT NULL,
+  PRIMARY KEY (idCostosD),
+    FOREIGN KEY(id_Presupuesto) REFERENCES presupuestos(idPresupuesto),
+	FOREIGN KEY(id_Mes) REFERENCES mes (idMes)
+)
+
+CREATE TABLE gastosAdmin
+(
+  idgastosAdmin INT NOT NULL IDENTITY (1,1),
+  id_Presupuesto INT NOT NULL,
+  id_Mes INT NOT NULL,
+  concepto FLOAT NOT NULL,
+  PRIMARY KEY (idgastosAdmin),
+    FOREIGN KEY(id_Presupuesto) REFERENCES presupuestos(idPresupuesto),
+	FOREIGN KEY(id_Mes) REFERENCES mes (idMes)
+)
