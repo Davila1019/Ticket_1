66e758e8051e8f0469208f4a90065b707845653d
AleAlvanez <42542259+AleAlvanez@users.noreply.github.com>
2021-10-08 00:10:09 -0500

    Vistas recursos,costos,resumen

---
+++ back/view/resourceView.js
@@ -0,0 +1,28 @@
+const cashFlowController = require('../controller/cashFlowController')
+const isAuthenticated = require('../middlewares/isAuthenticated')
+
+const jwt = require('jsonwebtoken');
+const {promisify} = require('util');
+module.exports = async (app) => {
+
+    app.get('/resource', async(req,res) => {
+        const decodificada = await promisify(jwt.verify)(req.cookies.budget, process.env.KEY)
+        const token = decodificada;
+        let data = await cashFlowController.get(token.name)
+
+        console.log(token)
+        console.log(data)
+        for(i=0; i<data.length; i++){
+            if(data[i].cashFlow[0] != undefined){
+                console.log(data[i].cashFlow[0])
+            }
+            
+        }
+        res.render('resource',{token, data})
+        
+    })
+
+    app.post('/resource', async(req,res)=>{
+        
+    })
+}
